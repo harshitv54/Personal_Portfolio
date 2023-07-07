2b0a66f37398993a4d9a7de14c8629edf3d7da26
Harshit <harshitverma54@gmail.com>
2023-07-07 12:57:27 +0530

    Add contact page elements & remove testimonials

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './contact.css'
+import {FiMail} from 'react-icons/fi'
+import {BiLogoTelegram} from 'react-icons/bi'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <FiMail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>harshitverma54@gmail.com</h5>
+            <a href="mailto:harshitverma54@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BiLogoTelegram className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Telegram</h5>
+            <a href="https://t.me/KonohaHokage" target='_blank'>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+91 95181 94534</h5>
+            <a href="https://wa.me/+919518194534" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */ }
+
+        <form action="">
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea name="message" rows="7" placeholder='Message' required ></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
